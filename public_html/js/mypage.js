a05e2e0698118c8c736fd9c51823ef788561bd1e
ITanCh <liutianchi92@gmail.com>
2013-12-02 13:10:14 +0800

    gettaskinfo

--- public_html/js/mypage.js
+++ public_html/js/mypage.js
@@ -4,6 +4,9 @@
  */
 
 var request;
+var taskrequest;
+//$(document).ready(checklogin);
+
 function checklogin(){
     if(window.XMLHttpRequest) {  
         request = new XMLHttpRequest();  //IE7, Firefox, Opera 
@@ -11,7 +14,7 @@ function checklogin(){
         requset = new ActiveXObject("Microsoft.XMLHTTP");   //IE5,IE6
     }
     if(request!==null){  
-            request.open("GET","getlogin",true);
+            request.open("GET","getuserinfo",true);
             request.onreadystatechange=checkback;
             request.send(null);
     }  
@@ -21,11 +24,64 @@ function checkback(){
     if(request.readyState===4){  
         if(request.status===200){  
             var flag=request.responseText;
-            if(flag==="true")return;
+            if(flag==="false"){              
+              location.href="index.html";        //this uer dosen't log in ,so he cannot get into this page
+            }
+            else{                        //log in successfully ,load user information          
+                var obj=eval('('+flag+')');
+                document.getElementById("username").innerHTML=obj.username;
+                document.getElementById("bigusername").innerHTML=obj.username;
+                document.getElementById("money").innerHTML=obj.coins;
+                document.getElementById("mail").innerHTML=obj.mail;
+                createtable();
+                return;
+            }
         }            
        location.href="index.html";        //this uer dosen't log in ,so he cannot get into this page
     }  
+}   
+
+
+function createtable(){
+    if(window.XMLHttpRequest) {  
+        taskrequest = new XMLHttpRequest();  //IE7, Firefox, Opera 
+    }else if(window.ActiveXObject) {  
+        taskrequset = new ActiveXObject("Microsoft.XMLHTTP");   //IE5,IE6
+    }
+    if(taskrequest!==null){  
+            taskrequest.open("GET","gettasks",true);
+            taskrequest.onreadystatechange=tasksback;
+            inforequest.send(null);
+    }  
 }
+
+function tasksback(){
+    if(inforequest.readyState===4){  
+        if(inforequest.status===200){  
+            var flag=taskrequest.responseText;
+            var obj=eval('('+flag+')');
+            var alltasks="";
+            for(var i=0,l=obj.task.length;i<l;i++){
+                //task:name ,create time, status
+                if(obj.task[i].isrunning===false){
+                    alltasks+="<tr><td>"+obj.task[i].name+"</td><td>"+obj.task[i].ctime+"</td><td>"+obj.task[i].status+"</td>\n\
+                           <td><a onclick='viewtask()'><i class='icon-eye-open tooltip' title='view and modify'></i></a>\n\
+                            <a class='starticon' href=''><i class='icon-play tooltip' title='start it'></i></a></td></tr>";
+                }
+                else{
+                    alltasks+="<tr><td>"+obj.task[i].name+"</td><td>"+obj.task[i].ctime+"</td><td>"+obj.task[i].status+"</td>\n\
+                           <td><a href=''><i class='icon-eye-open tooltip' title='view and modify'></i></a>\n\
+                            <a class='stopicon' href=''><i class='icon-stop tooltip' title='stop it'></i></a></td></tr>";
+                }
+            }
+            document.getElementById("tablebody").innerHTML=alltasks;
+        }            
+    }  
+}  
+
+
+
+
 
 
 
