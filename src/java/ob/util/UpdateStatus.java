6647ea4131b26de2967b0e625ba0643c35a4db3f
oubeichen <oubeichen@gmail.com>
2013-12-17 21:33:55 +0800

    Inital test

---
+++ src/java/ob/util/UpdateStatus.java
@@ -0,0 +1,19 @@
+package ob.util;
+
+import weibo4j.Timeline;
+import weibo4j.model.Status;
+import weibo4j.model.WeiboException;
+/*
+ * 摘自微博API用于发送微博
+ */
+public class UpdateStatus {
+
+	public static String Update(String access_token,String statuses) throws WeiboException {
+
+		Timeline tm = new Timeline();
+		tm.client.setToken(access_token);
+		Status status = tm.UpdateStatus(statuses);
+		return status.toString();
+        }
+
+}
