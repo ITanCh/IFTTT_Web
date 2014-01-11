0c770bb42b8c68aa559f32c4460931cab1cb17ee
oubeichen <oubeichen@gmail.com>
2014-01-11 12:49:18 +0800

    Use ConcurrentHashMap Instead. Because there is no iterator , it is useless to use synchronized block.

--- src/java/ob/manager/RunningTask.java
+++ src/java/ob/manager/RunningTask.java
@@ -3,51 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package ob.manager;
 
 import ob.PO.TaskPO;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
  * @author oubeichen
  */
 public class RunningTask {
-
-    private static final HashMap<String, TaskPO> tasks = new HashMap<String, TaskPO>();
-
-    public static boolean addTask(TaskPO task) {
-        synchronized (tasks) {
-            tasks.put(task.getTid(), task);
-            task.setIsrunning(true);
-            task.start();
-        }
+    private static final ConcurrentHashMap<String,TaskPO> tasks = new ConcurrentHashMap<String,TaskPO>();
+    public static boolean addTask(TaskPO task){
+        tasks.put(task.getTid(),task);
+        task.setIsrunning(true);
+        task.start();
         return true;
     }
-
-    public static boolean isHere(String tid) {
-        synchronized (tasks) {
-            return tasks.containsKey(tid);
-        }
+    public static boolean isHere(String tid){
+        return tasks.containsKey(tid);
     }
-
-    public static String getStatus(String tid) {
-        synchronized (tasks) {
-            if (tasks.containsKey(tid)) {
-                return tasks.get(tid).getStatus();
-            }
+    public static String getStatus(String tid){
+        if(tasks.containsKey(tid)){
+            return tasks.get(tid).getStatus();
         }
         return null;
     }
-
-    public static boolean delTask(String tid) {
-        synchronized (tasks) {
-            if (tasks.containsKey(tid)) {
-                TaskPO po = tasks.get(tid);
-                po.setIsrunning(false);
-                tasks.remove(tid);
-                return true;
-            }
+    public static boolean delTask(String tid){
+        if(tasks.containsKey(tid)){
+            TaskPO po = tasks.get(tid);
+            po.setIsrunning(false);
+            tasks.remove(tid);
+            return true;
         }
         return false;
     }
